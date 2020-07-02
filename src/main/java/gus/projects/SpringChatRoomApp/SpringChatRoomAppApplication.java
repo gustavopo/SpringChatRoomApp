5871c8ace5cbf3b74795df0209c154f474ab9f21
Gustavo <gusta.p.o@hotmail.com>
2020-07-02 14:14:23 +0100

    Initial commit

---
+++ src/main/java/gus/projects/SpringChatRoomApp/SpringChatRoomAppApplication.java
@@ -0,0 +1,13 @@
+package gus.projects.SpringChatRoomApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChatRoomAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringChatRoomAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gus/projects/SpringChatRoomApp/SpringChatRoomAppApplicationTests.java
@@ -0,0 +1,13 @@
+package gus.projects.SpringChatRoomApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringChatRoomAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
